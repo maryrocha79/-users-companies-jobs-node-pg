dc8db91700c5b41de39786cd9e0771db60d359a0
kelley <kelleylsharp@gmail.com>
2018-07-11 18:07:02 -0700

    updated company_id to handle

--- routes/companies.js
+++ routes/companies.js
@@ -14,8 +14,14 @@ router.post('', async function(req, res, next) {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const data = await db.query(
-      'insert into companies (name,logo,handle,password) values($1,$2,$3,$4) returning *',
-      [req.body.name, req.body.logo, req.body.handle, hashedPassword]
+      'insert into companies (name,email,logo,handle,password) values($1,$2,$3,$4,$5) returning *',
+      [
+        req.body.name,
+        req.body.email,
+        req.body.logo,
+        req.body.handle,
+        hashedPassword
+      ]
     );
     return res.json(data.rows[0]);
   } catch (err) {
@@ -34,21 +40,21 @@ router.get('', checkLoggedIn, async function(req, res, next) {
 });
 
 // GET /companies/:id - this should return a single company found by its id
-router.get('/:id', checkLoggedIn, async function(req, res, next) {
+router.get('/:handle', checkLoggedIn, async function(req, res, next) {
   try {
-    const companyData = await db.query('select * from companies where id=$1', [
-      req.params.id
+    const companyData = await db.query(
+      'select * from companies where handle=$1',
+      [req.params.handle]
+    );
+    const employees = await db.query(
+      'select users.username from users where current_company=$1',
+      [req.params.handle]
+    );
+    const jobs = await db.query('select jobs.id from jobs where handle=$1', [
+      req.params.handle
     ]);
-    const users = await db.query(
-      'select users.id from users where current_company_id=$1',
-      [req.params.id]
-    );
-    const jobs = await db.query(
-      'select jobs.id from jobs where company_id=$1',
-      [req.params.id]
-    );
 
-    companyData.rows[0].users = users.rows.map(val => val.id);
+    companyData.rows[0].employees = employees.rows.map(val => val.username);
     companyData.rows[0].jobs = jobs.rows.map(val => val.id);
     return res.json(companyData.rows[0]);
   } catch (err) {
@@ -56,17 +62,17 @@ router.get('/:id', checkLoggedIn, async function(req, res, next) {
   }
 });
 // PATCH /companies/:id - this should update an existing company and return the updated company
-router.patch('/:id', checkCorrectCompany, async function(req, res, next) {
+router.patch('/:handle', checkCorrectCompany, async function(req, res, next) {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const data = await db.query(
-      'update companies set name=$1,logo=$2,handle=$3,password=$4 where id=$5 returning *',
+      'update companies set email=$1,logo=$2,handle=$3,password=$4 where handle=$5 returning *',
       [
-        req.body.name,
+        req.body.email,
         req.body.logo,
         req.body.handle,
         hashedPassword,
-        req.params.id
+        req.params.handle
       ]
     );
     return res.json(data.rows[0]);
@@ -75,11 +81,11 @@ router.patch('/:id', checkCorrectCompany, async function(req, res, next) {
   }
 });
 // DELETE /companies/:id - this should remove an existing company and return the deleted company
-router.delete('/:id', checkCorrectCompany, async function(req, res, next) {
+router.delete('/:handle', checkCorrectCompany, async function(req, res, next) {
   try {
     const data = await db.query(
-      'delete from companies where id=$1 returning *',
-      [req.params.id]
+      'delete from companies where handle=$1 returning *',
+      [req.params.handle]
     );
     return res.json(data.rows[0]);
   } catch (err) {
@@ -105,7 +111,7 @@ router.post('/auth', async (req, res, next) => {
     } else {
       const token = jsonwebtoken.sign(
         {
-          company_id: foundCompany.rows[0].id
+          handle: foundCompany.rows[0].handle
         },
         'SECRETK'
       );
